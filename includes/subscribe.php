b7d756d2b9868fe12e45f754d0bac7fa246d7007
Sami Keijonen <sami.keijonen@foxnet.fi>
2012-12-31 14:21:22 +0200

    Add different support timesets for different purchases. How cool is this!

--- includes/subscribe.php
+++ includes/subscribe.php
@@ -20,15 +20,79 @@ function my_edd_stuff_add_expire_date( $payment_id, $new_status, $old_status ) {
 		return;
 
 	/* Get current user id. */
-	$payment_data 	= get_post_meta( $payment_id, '_edd_payment_meta', true );
-	$user_info 	= maybe_unserialize( $payment_data['user_info'] );
+	$payment_data = get_post_meta( $payment_id, '_edd_payment_meta', true );
+	$user_info = maybe_unserialize( $payment_data['user_info'] );
 	$user_id = $user_info['id'];
 
+	/* Get cart items. */
+	$cart_items = maybe_unserialize( $payment_data['cart_details'] );
+	
+	foreach( $cart_items as $key => $cart_item ) {
+		
+		/* Retrieve the ID of the download. */
+		$id = $cart_item['id'];
+		
+		/* Retrieve the price_id. */
+		$price_options = $cart_items[$key]['item_number']['options'];
+																															
+		if( isset( $price_options['price_id'] ) ) {
+			
+			/* Get price id. This is 0, 1, 2 etc. Use this. */
+			$price_id = $price_options['price_id'];
+			
+			/* Get price name. This is Basic, Standard or Developer. This is just if needed. Use price_id. */
+			$price_name = edd_get_price_option_name( $id, $price_options['price_id'] );
+		
+		}
+	
+	}
+	
+	/* Support time is based on price_id. */
+	if ( $price_id == 0 ) {
+		$support_time = '+6 month';
+	}
+	else if ( $price_id == 1 ) {
+		$support_time = '+1 year';
+	}
+	else if ( $price_id == 2 ) {
+		$support_time = '+2 year';
+	}
+	else {
+		$support_time = '+2 year';
+	}
+	
 	/* Current date. */
 	$current_date = date( 'Y-m-d' );
+		
+	/* Get expire date. */
+	$expire_date = get_user_meta( $user_id, 'expire_date', true );
 	
-	/* Add one year. */
-	$expire_date = strtotime ( '+1 year' , strtotime ( $current_date ) ) ;
+	/* If expire_date is not set ( this means new user), add current_date + support_time. Else there is current_date already. */
+	if ( !isset( $expire_date ) ) {
+		$expire_date = strtotime ( $support_time , strtotime ( $current_date ) ) ;
+	}
+	else {
+		
+		/* if expire_date < current_date, add current_date + support_time. */
+		if ( $expire_date < strtotime ( $current_date ) ) {
+			$expire_date = strtotime ( $support_time , strtotime ( $current_date ) );
+		}
+		else {
+			
+			/* Get future_date so that we can check should we do anything to current expire_date. Else add current_date + support_time. */
+			$future_date = strtotime ( $support_time , strtotime ( $current_date ) );
+			
+			/* If future_date < expire_date, don't add anything. */
+			if ( $future_date < $expire_date ) {
+				$expire_date = $expire_date;
+			}
+			else {
+				$expire_date = strtotime ( $support_time , strtotime ( $current_date ) );
+			}
+			
+		}
+		
+	}
 	
 	/* Update user meta. */
 	update_user_meta( $user_id, 'expire_date', $expire_date );
